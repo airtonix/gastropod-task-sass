2d2a9b33d23bd9f58f9e52404725f686652999f1
Zenobius Jiricek <airtonix@gmail.com>
2016-03-08 08:28:57 +1030

    add asset url functions

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 /**
  * System
  */
-var path = require('path');
+var path = require('path'),
+	url = require('url');
 
 /**
  * Framework
  */
-var sass = require('gulp-sass'),
+var gulpSass = require('gulp-sass'),
+	sass = require('gulp-sass/node_modules/node-sass'),
 	sourcemaps = require('gulp-sourcemaps'),
 	debug = require('debug')('gastropod/addons/tasks/sass'),
 	plumber = require('gulp-plumber'),
@@ -15,13 +17,13 @@ var sass = require('gulp-sass'),
 	accept = require('check-args');
 
 /**
-  * Project
-  */
+ * Project
+ */
 var DEFAULT_CONFIG = {
 	errLogToConsole: true,
-	includePaths: []
+	includePaths: [],
+	functions: []
 }
-
 
 var buildPath = accept(String, String, String)
 				.to(function(root, staticRoot, section){
@@ -35,17 +37,25 @@ var buildPath = accept(String, String, String)
  * Exportable
  */
 module.exports = function (gulp, gastro){
+	var logger = gastro.Logging.Logger('Sass');
+	var PluginError = gastro.Plugins.util.PluginError;
+
 	/**
 	 * Constants
 	 */
 	function ErrorHandler (err) {
-		gastro.Plugins.util.PluginError('task-sass', err, {
+		var error = new PluginError('scss', err, {
 			showStack: true
 		});
+		gastro.Plugins.util.log(error.formatted);
 		this.emit('end');
+		return
 	}
 
 	var Config = gastro.Config,
+		Urls = Config.context.Site.urls,
+		MediaRoot = path.join(Urls.root, Urls.media),
+		StaticRoot = path.join(Urls.root, Urls.static),
 
 		target = buildPath(Config.target.root,
 						   Config.target.static,
@@ -57,6 +67,22 @@ module.exports = function (gulp, gastro){
 
 		SassConfig = Config.plugins.css.sass || DEFAULT_CONFIG;
 
+		debug('MediaRoot', MediaRoot);
+		debug('StaticRoot', StaticRoot);
+
+		SassConfig.functions = {
+
+			'media-url($url)': function (url, done) {
+				var output = path.join(MediaRoot, url.getValue());
+				done(new sass.types.String(output || 'error'));
+			},
+
+			'static-url($url)': function (url, done) {
+				var output = Manifest.db[url.getValue()] || url;
+				done(new sass.types.String(output || 'error'));
+			},
+
+		}
 	gulp.task('scss', function(done){
 
 		debug('Starting');
@@ -67,7 +93,7 @@ module.exports = function (gulp, gastro){
 			.pipe(plumber(ErrorHandler))
 			.pipe(named())
 			.pipe(sourcemaps.init())
-			.pipe(sass(SassConfig))
+			.pipe(gulpSass(SassConfig).on('error', ErrorHandler))
 			.pipe(sourcemaps.write('./maps'))
 			.pipe(gulp.dest(target));
 
